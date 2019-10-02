8ec660ba21c24d3d75434b30e98922b10ce5ca10
anderson.cabezas <anderson.cabezas@pragma.com.co>
2019-10-02 17:14:49 -0500

    Se elimina importacion swagger

--- DevopsPragmaPractica/src/main/java/com/practica/devops/PracticaDevOpsApplication.java
+++ DevopsPragmaPractica/src/main/java/com/practica/devops/PracticaDevOpsApplication.java
@@ -3,9 +3,7 @@ package com.practica.devops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Profile;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 
